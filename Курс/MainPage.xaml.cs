fe0b4dac27707eb6eb2e8d0d77e01c351aa90e1f
Alexander Gribanov <86486142+XanderJB@users.noreply.github.com>
2021-12-05 18:55:10 +0300

    Update MainPage.xaml.cs

--- Курс/MainPage.xaml.cs
+++ Курс/MainPage.xaml.cs
@@ -1,18 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
+using System;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
 
-namespace App2
+namespace XamarinApp1
 {
-    public partial class MainPage : ContentPage
-    {
-        public MainPage()
-        {
-            InitializeComponent();
-        }
-    }
+	[DesignTimeVisible(false)]
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Clicked(object sender, EventArgs e) //Обработчик нажатия на кнопку
+		{
+			try
+			{
+				int n1 = Convert.ToInt32(num1.Text); //Конвертация значений из текстовых полей в числа
+				int n2 = Convert.ToInt32(num2.Text);
+
+				result.Text = (n1 + n2).ToString(); //Сложение полученных чисел, преобразование их обратно в строку и вывод в один из элементов <Label>
+			}
+			catch (Exception exc) { }
+		}
+	}
 }
